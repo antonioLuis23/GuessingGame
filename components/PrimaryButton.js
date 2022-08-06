9c5c793da14762ac917fd17eaba80e83d5c94f65
Antonio Luis <antonio.silva@arctouch.com>
2022-08-06 00:22:22 -0300

    feat: Create main screen

---
+++ components/PrimaryButton.js
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View, Pressable } from "react-native";
+
+const PrimaryButton = ({ children }) => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={() => {
+          console.log("PRESSED!");
+        }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
+export default PrimaryButton;
